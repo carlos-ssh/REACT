70c66d5481a9679ef115ff025206959b1397a208
Carlos <carlos.anezeh@gmail.com>
2021-12-02 09:53:22 -0600

    Add style for complete items and the function that display this condition if it finished a task.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import './TodoList.css';
 
 const Todo = ({ text, todo, todos, setTodos }) => {
   const deleteHandler = () => {
-    console.log(todo);
     setTodos(todos.filter(el => el.id !== todo.id));
   };
+  const completeHandler = () => {
+    setTodos(todos.map((item) => {
+      if (item.id === todo.id) {
+        return {
+          ...item, completed: !item.completed
+        }
+      }
+      console.log(item);
+      return item;
+    }));
+  }
 
   return (
     <div className="todo">
-      <li className="todo-item">{ text }</li>
-      <button className="done">Complete</button>
-      <button onClick={ deleteHandler } className="del">Remove</button>
+      <li className={`todo-item ${todo.completed ? 'active' : ""}`}>{ text }</li>
+      <button onClick={completeHandler} className="done">Complete</button>
+      <button onClick={deleteHandler} className="del">Remove</button>
     </div>
   );
 }
