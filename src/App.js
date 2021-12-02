906579a842e35fd2ded2cca3dcdb17311f5b4c2f
Carlos <carlos.anezeh@gmail.com>
2021-12-02 13:14:28 -0600

    Add local storage.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ function App() {
   const [filteredTodos, setFilteredTodos] = useState([]);
 
   useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
   const filterHandler = () => {
@@ -31,7 +36,20 @@ function App() {
       }
     }
   };
-  console.log(filteredTodos);
+
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    }else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
+    }
+  }
+
   return (
     <div className="App">
       <Nav />
